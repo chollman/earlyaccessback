c761db39766117ce274dc54a135bd91260cd6695
Claudio Hollman <claudiohollman@gmail.com>
2022-11-18 00:57:02 -0300

    Agregado modelo de articulo

---
+++ models/articles.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const articleSchema = new Schema(
+  {
+    title: { type: String, trim: true, required: true, unique: true },
+    description: { type: String, trim: true },
+    content: { type: String, trim: true },
+    author: { type: String, trim: true },
+    publicationDate: { type: Date },
+    image: { data: Buffer, contentType: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('article', articleSchema);
