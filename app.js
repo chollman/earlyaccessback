367dbef2fd5b0292dc8f84e59e3b192d7cdb6456
Claudio Hollman <claudiohollman@gmail.com>
2022-11-23 17:12:17 -0300

    Agregada seguridad

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
 
+// SECURITY
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // EXPRESS
 const express = require('express')
 const app = express()
@@ -8,7 +14,18 @@ const app = express()
 // MIDDLEWARE
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowsMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // DATABASE
 const connectDB = require('./db/connect')
