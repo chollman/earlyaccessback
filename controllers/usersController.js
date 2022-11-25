dcf0e4f12f1c2199f9328bf780d8cff629b954b8
Claudio Hollman <claudiohollman@gmail.com>
2022-11-25 19:01:13 -0300

    usersController Part 1

---
+++ controllers/usersController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+const getAllUsers = async (req, res) => {
+  res.send('get all users')
+}
+const getSingleUser = async (req, res) => {
+  res.send('get single users')
+}
+const showCurrentUser = async (req, res) => {
+  res.send('show current user')
+}
+const updateUser = async (req, res) => {
+  res.send('update user')
+}
+const updateUserPassword = async (req, res) => {
+  res.send('update user password')
+}
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+}
---
+++ routes/usersRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} = require('../controllers/usersController')
+
+router.route('/').get(getAllUsers)
+router.route('/showme').get(showCurrentUser)
+router.route('/updateUser').patch(updateUser)
+router.route('/updateUserPassword').patch(updateUserPassword)
+router.route('/:id').get(getSingleUser)
+
+module.exports = router
